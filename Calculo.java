8d4d300174301e9904779cbe760f929bfd494837
Smile <31359192+HurBizo@users.noreply.github.com>
2017-10-31 21:55:26 -0200

    Create Calculo.java

---
+++ Calculo.java
@@ -0,0 +1,21 @@
+package pct;
+
+public class Calculo {
+    double n1;
+    double n2;
+    double n3;
+    double res;
+           
+    
+    
+    public void imprimirConta(){
+        System.out.println("");
+}
+    
+    public double EquacaoPrimeiro(){
+        
+        
+        return 0.0;
+    }
+    
+}
