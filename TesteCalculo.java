7f31a6da2366dc75988af4c6cbac261f36faa7e5
Smile <31359192+HurBizo@users.noreply.github.com>
2017-10-31 21:54:49 -0200

    Create TesteCalculo.java

---
+++ TesteCalculo.java
@@ -0,0 +1,26 @@
+
+package pct;
+
+import java.util.Scanner;
+
+public class TesteCalculo {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        Calculo calculo = new Calculo();
+    
+    int[] n;
+    n = new int[3]; 
+    
+    for (int i = 0; i < n.length; i++ ){
+        System.out.println((i + 1) + "º Pergunta!");
+        n[i] = entrada.nextInt();
+        
+        
+        }
+    for (int i = 0; i < n.length ; i++){
+        
+    }
+    
+    }
+    
+}
